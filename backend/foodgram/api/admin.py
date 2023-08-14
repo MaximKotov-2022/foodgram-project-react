a94813ab44ec1b00398987de1807f2763af9c2c4
Kotov Maxim <KM4252010@yandex.ru>
2023-08-14 08:48:56 +0300

    Add user search to the admin area.

--- backend/foodgram/api/admin.py
+++ backend/foodgram/api/admin.py
@@ -6,7 +6,7 @@ from .models import Follow, User
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'email', 'username', 'first_name', 'last_name',)
-    list_filter = ('username', 'email',)
+    search_fields = ('username', 'email',)
 
 
 @admin.register(Follow)
