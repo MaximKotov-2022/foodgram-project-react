7260b5b52576453d3b7696a2940387b4a59004dc
Kotov Maxim <KM4252010@yandex.ru>
2023-09-01 17:56:43 +0300

    test settings

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', "4y-q*uq1!4$-89$")
 # DEBUG = os.getenv('DEBUG', '').lower() == 'true'
 DEBUG = True
 ALLOWED_HOSTS = ['158.160.0.169', '127.0.0.1', 'localhost', 'myfoodgram.zapto.org']
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1").split()
+# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1").split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
